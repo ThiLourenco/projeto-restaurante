a44b97baee5ca0ede262efcae3558bc729a59cce
Thiago-l <thigo.l17@yahoo.com>
2020-10-22 19:44:10 -0300

    new project

---
+++ routes/users.js
@@ -0,0 +1,21 @@
+const conn = require('./../database/db');
+const express = require('express');
+const router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  
+  conn.query("SELECT * FROM tb_users ORDER BY name", (err, result) => {
+
+    if(err) {
+      res.send(err);
+
+    } else {
+      res.send(result);
+    }
+
+  });
+
+});
+
+module.exports = router;
