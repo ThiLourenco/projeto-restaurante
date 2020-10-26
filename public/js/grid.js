fa4adecd451ecc74832090a7829db981ddc771a7
Thiago-l <thigo.l17@yahoo.com>
2020-10-26 18:40:08 -0300

    creating component grid for events abstration

--- public/js/grid.js
+++ public/js/grid.js
@@ -2,6 +2,14 @@ class Grid {
     
   constructor(configs) {
 
+      configs.listeners = Object.assign({
+          afterUpdateClick: (e)=> {
+
+            $('#modal-update').modal('show');
+            
+          }
+      }, configs.listeners);
+
       this.options = Object.assign({}, {
         formCreate: '#modal-create form',
         formUpdate: '#modal-update form',
@@ -39,12 +47,20 @@ class Grid {
 
   }
 
+  fireEvent(name, args) {
+
+    if (typeof this.options.listeners[name] === 'function') this.options.listeners[name].apply(this, args);
+
+  }
+
   initButtons() {
 
     
     [...document.querySelectorAll(this.options.btnUpdate)].forEach(btn => {
 
       btn.addEventListener('click', e => {
+        
+        this.fireEvent('beforeUpdateClick', [e]);
         
         let tr = e.path.find(el => {
 
@@ -69,7 +85,7 @@ class Grid {
 
         }
 
-        $('#modal-update').modal('show');
+        this.fireEvent('afterUpdateClick', [e]);
 
       });
 
