d402f5e514f99ae175a20220e488b93c2a7f4778
Thiago-l <thigo.l17@yahoo.com>
2020-10-26 21:31:27 -0300

    user administration

--- inc/users.js
+++ inc/users.js
@@ -2,9 +2,26 @@ const conn = require('./db');
 
 module.exports = {
 
+  getUsers() {
+    
+    return new Promise((resolve, reject) => {
+
+      conn.query(`
+          SELECT * FROM tb_users ORDER BY name 
+      `, (err, results) => {
+  
+      if(err) {
+        reject(err);
+      }
+      resolve(results);  
+      
+      });
+    });
+  },
+
   render(req, res, error) {
 
-    res.render('admin/login', {
+    res.render('admin/users', {
         body: req.body,
         error
     });
@@ -42,6 +59,73 @@ module.exports = {
           }
 
         }
+
+      });
+
+    });
+
+  },
+
+  save(fields, files) {
+
+    return new Promise((resolve, reject) => {
+
+      let query, params = [
+        fields.name,
+        fields.email
+      ];
+
+      
+      if (parseInt(fields.id) > 0) {
+
+        params.push(fields.id);
+
+        query = `
+            UPDATE tb_users
+              SET name = ?,
+                  email = ?
+              WHERE id = ?
+        `;
+
+      } else {
+
+        query = `
+            INSERT INTO tb_users (name, email, password)
+            VALUES(?, ?, ?)
+        `;
+        
+        params.push(fields.password);
+
+      }
+
+      conn.query(query, params, (err, results)=>{
+
+        if(err) {
+          reject(err)
+        } else {
+          resolve(results);
+        }
+
+      });
+
+    });
+  },
+
+  delete(id) {
+
+    return new Promise((resolve, reject) => {
+
+      conn.query(`
+         DELETE FROM tb_users WHERE id = ?
+      `, [
+        id
+      ], (err, results) => {
+        
+        if(err) {
+          reject(err);
+        } else {
+          resolve(results);
+      }
 
       });
 
