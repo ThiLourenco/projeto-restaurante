27b5a2acc4a6f3008abbac97627ad1f9eabf9ab3
Thiago-l <thigo.l17@yahoo.com>
2020-10-28 16:28:59 -0300

    added socket.io

--- app.js
+++ app.js
@@ -6,12 +6,21 @@ var logger = require('morgan');
 var session = require('express-session');
 var RedisStore = require('connect-redis')(session);
 var formidable = require('formidable');
-
+var http = require('http');
+var socket = require('socket.io');
 var indexRouter = require('./routes/index');
 var adminRouter = require('./routes/admin');
 
 var app = express();
 
+var http = http.Server(app);
+var io = socket(http);
+
+io.on('connection', function(socket) {
+
+  console.log('Novo usuário conectado!');
+
+});
 
 app.use(function(req, res, next) {
 
@@ -75,6 +84,12 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+http.listen(8080, function(){
+
+  console.log('Servidor em execução');
+
 });
 
 module.exports = app;
