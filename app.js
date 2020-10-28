b482099b9591511b8076e97fd479ec653218708a
Thiago-l <thigo.l17@yahoo.com>
2020-10-28 19:13:54 -0300

    dashboard with socket.io

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ var RedisStore = require('connect-redis')(session);
 var formidable = require('formidable');
 var http = require('http');
 var socket = require('socket.io');
-var indexRouter = require('./routes/index');
-var adminRouter = require('./routes/admin');
+
 
 var app = express();
 
@@ -21,6 +20,9 @@ io.on('connection', function(socket) {
   console.log('Novo usuário conectado!');
 
 });
+
+var indexRouter = require('./routes/index')(io);
+var adminRouter = require('./routes/admin')(io);
 
 app.use(function(req, res, next) {
 
